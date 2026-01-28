d28f6b1eb37407979d800f1147fcdc802082ee8d
DanielleJusto <danielleejusto@gmail.com>
2026-01-28 11:28:42 -0500

    added student attributes

--- student.java
+++ student.java
@@ -1,3 +1,31 @@
-public class student {
-    
+public class Student {
+    String name;
+    String email;
+    String major;
+    String minor;
+    int classYear;
+    int gradYear;
+    int id;
+    // Completed Courses
+    // Requirements
+    // Advisor
+    int creditsEnrolled;
+    int creditsCompleted;
+    double gpa;
+    boolean holdActive;
+
+    public Student(){
+        this.name = null;
+        this.email = null;
+        this.major = null;
+        this.minor = null;
+        this.gradYear = 0;
+        this.classYear = 0;
+        this.id = 0;
+        this.creditsEnrolled = 0;
+        this.creditsCompleted = 0;
+        this.gpa = 0.00;
+        this.holdActive = false;
+    }
+
 }
