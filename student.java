2c1c94dd4a7c55e70f291f9ffa2adbf3b3e6daf2
jazminemsjordan <jazminejordan77@gmail.com>
2026-01-28 23:51:22 -0500

    - changed some names, setter return values, and other tweaks to meet conventions - imported ArrayList and used it for collections so we can alter them in ways we can't built-in ArrayList - added a few getter methods, including program of study - added setter methods register(), drop(), and declareMajor()

--- student.java
+++ student.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 // Student Class due 1/29/26
 // CSC230 
 public class Student {
@@ -11,8 +12,9 @@ public class Student {
     int classYear;
     int gradYear;
     int id;
-    Course[] completedCourses; // Completed Courses - dictionary of Courses
-    Course[] requirements; // Requirements - dictionary of Courses - get from major, minor
+    ArrayList<Course> coursesCompleted; // Completed Courses - dictionary of Courses
+    ArrayList<Course> coursesEnrolled; // array of currently entrolled courses
+    ArrayList<Course> requirements; // Requirements - dictionary of Courses - get from major, minor
     Instructor advisor; // Advisor - type Instructor / Faculty / whatever name we're going with
     int creditsEnrolled;
     int creditsCompleted;
@@ -34,14 +36,78 @@ public class Student {
         this.creditsCompleted = 0;
         this.gpa = 0.00;
         this.holdActive = false;
+        this.coursesCompleted = new ArrayList<>();
+        this.coursesEnrolled = new ArrayList<>();
+        this.requirements = new ArrayList<>();
     }
+
     // Methods
     //Getters
     public String getName() { return name; }
     public String getEmail() { return email; }
+    public int getId() { return this.id; }
     public boolean holdActive() { return holdActive; }
+    public double getGpa() { return this.gpa; }
+    public int getGradYear() { return this.gradYear; }
+    public String getProgramOfStudy() {
+        if (this.major1 == null){
+            return("undeclared");
+        } else if (this.major2 == null){
+            if (this.minor == null){
+                return this.major1;
+            } else {
+                return this.major1 + ", " + this.minor;
+            }
+        } else if (this.minor == null){
+            return this.major1 + ", " + this.major2;
+        } else {
+            return this.major1 + ", " + this.major2 + ", " + this.minor;
+        }
+    }
 
     //Setters
-    public Instructor advisor(Instructor newAdvisor) { this.advisor = newAdvisor; }
-    public boolean holdActive(boolean updateHold) { this.holdActive = updateHold; }
+    public void setAdvisor(Instructor newAdvisor) { this.advisor = newAdvisor; }
+    public void setActiveHold(boolean updateHold) { this.holdActive = updateHold; }
+    /** 
+     * Method to declare a major
+     * @return true or false, whether declaration was successful
+     */
+    public boolean declareMajor(String major){
+        if (this.major1 == null){
+            this.major1 = major;
+            return true;
+        } else if (this.major2 == null) {
+            this.major2 = major;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * Method to register for a course
+     * @return true or false if registration was successful
+     */
+    public boolean register(Course course){
+        if (this.holdActive == true){
+            return false;
+        } else {
+            this.coursesEnrolled.add(course);
+            this.creditsEnrolled = this.creditsEnrolled + course.getCreditHours;
+            return true;
+        }
+    }
+    /**
+     * Method to drop a course
+     * @return true or false, whether the drop was successful
+     */
+    public boolean drop(Course course){
+        // check if student is enrolled and can drop
+        if (this.coursesEnrolled.contains(course)){
+            this.coursesEnrolled.remove(course);
+            this.creditsEnrolled = this.creditsEnrolled - course.getCreditHours();
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
